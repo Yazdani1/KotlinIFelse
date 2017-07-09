0b873029d14098a2ef882fd963fcc36cdcd49258
Yazdani <yaz4dani@gmail.com>
2017-07-09 22:06:30 +0600

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by Yazdani on 7/9/2017.
+ */
+
+fun main(args : Array<String>){
+
+    var a=100
+    var b=150
+    var mlarge=if(a>b) a else b
+
+    println("Largest Number is ${mlarge}")
+
+}
